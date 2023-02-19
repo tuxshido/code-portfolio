e4aa4475a753a81eb9981c59f4b35dc0db2aa290
tuxshido <c.metman@e.email>
2023-02-20 08:45:26 +0900

    [fix] path issue

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route
-                    exact
-                    path="/VSCode-portfolio/"
-                    element={<Home />}></Route>
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/code-portfolio/" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
